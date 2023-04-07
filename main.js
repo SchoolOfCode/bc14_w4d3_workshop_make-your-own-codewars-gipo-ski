fc4854cec567ad81f3028b9749266405534004de
gipo-ski <koladiji@gmail.com>
2023-04-07 20:02:13 +0100

    completed step 1 and step 2 of make-your-own-codewars-recap-tasks

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
 /* 
 👉 Write your kata here!
+"And the winner of the 'gipo-ski award' is..."
+
+you are the host of big-g show. its your job to announce the winner of the 'gipo-ski award'.
+there are 8 possible winners: Michael, Abigail, Sofia, Sebastian, Noah, Evangeline, David or Kunle.
+only one person could be named winner.
+
+you have chosen to name the person whose name comes first alphabetically as the winner.
+
+write a function that returns the 'winner' from the 8 'nominees' names.
+the winner is announced in this format: 'And the winner of the 'gipo-ski award' is "winner-name-goes-here"!'
+the first 3 letters of winner's name should be in capital letters.
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+export function andTheWinnerIs() {
+  //write your code here.
+  //store nominees in an array
+  const nominees = [
+    "Michael",
+    "Abigail",
+    "Sofia",
+    "Sebastian",
+    "Noah",
+    "Evangeline",
+    "David",
+    "Kunle",
+  ];
+  //sort the array
+  const nomineesSorted = nominees.sort();
+  //slice() winner name after 3rd character of name
+  const restOfWinnerName = nomineesSorted[0].slice(3);
+  //slice() 1st 3 characters of winner name, capitalise it and concatinate restOfWinnerName to it
+  const winner = nomineesSorted[0].slice(0, 3).toUpperCase() + restOfWinnerName;
+  //get result
+  return `And the winner of the 'gipo-ski award' is ${winner}!`;
+}
+// winner = andTheWinnerIs();
+console.log(andTheWinnerIs());
--- main.test.js
+++ main.test.js
@@ -1,1 +1,84 @@
 //👉 Write your tests below here:
+import { test, expect } from "@jest/globals";
+import { andTheWinnerIs } from './main.js';
+
+//test for winner not to be equal to 'Michael'.
+test("when andTheWinnerIs is called, 'And the winner of the 'gipo-ski award' is Michael!' should NOT be displayed ", function () {
+  //arrange
+  const actual = andTheWinnerIs();
+  const expected = "And the winner of the 'gipo-ski award' is Michael!";
+  //assert
+  expect(actual).not.toBe(expected);
+});
+
+//test for winner not to be equal to 'Abigail'.
+test("when andTheWinnerIs is called, 'And the winner of the 'gipo-ski award' is Abigai!' should NOT be displayed ", function () {
+  //arrange
+  const actual = andTheWinnerIs();
+  const expected = "And the winner of the 'gipo-ski award' is Abigail!";
+  //assert
+  expect(actual).not.toBe(expected);
+});
+
+//test for winner not to be equal to 'Sofia'.
+test("when andTheWinnerIs is called, 'And the winner of the 'gipo-ski award' is Sofia!' should NOT be displayed ", function () {
+  //arrange
+  const actual = andTheWinnerIs();
+  const expected = "And the winner of the 'gipo-ski award' is Sofia!";
+  //assert
+  expect(actual).not.toBe(expected);
+});
+
+//test for winner not to be equal to 'Sebastian'.
+test("when andTheWinnerIs is called, 'And the winner of the 'gipo-ski award' is Sebastian!' should NOT be displayed ", function () {
+  //arrange
+  const actual = andTheWinnerIs();
+  const expected = "And the winner of the 'gipo-ski award' is Sebastian!";
+  //assert
+  expect(actual).not.toBe(expected);
+});
+
+//test for winner not to be equal to 'Noah'.
+test("when andTheWinnerIs is called, 'And the winner of the 'gipo-ski award' is Noah!' should NOT be displayed ", function () {
+  //arrange
+  const actual = andTheWinnerIs();
+  const expected = "And the winner of the 'gipo-ski award' is Noah!";
+  //assert
+  expect(actual).not.toBe(expected);
+});
+
+//test for winner not to be equal to 'Evangeline'.
+test("when andTheWinnerIs is called, 'And the winner of the 'gipo-ski award' is Evangeline!' should NOT be displayed ", function () {
+  //arrange
+  const actual = andTheWinnerIs();
+  const expected = "And the winner of the 'gipo-ski award' is Evangeline!";
+  //assert
+  expect(actual).not.toBe(expected);
+});
+
+//test for winner not to be equal to 'David'.
+test("when andTheWinnerIs is called, 'And the winner of the 'gipo-ski award' is David!' should NOT be displayed ", function () {
+  //arrange
+  const actual = andTheWinnerIs();
+  const expected = "And the winner of the 'gipo-ski award' is David!";
+  //assert
+  expect(actual).not.toBe(expected);
+});
+
+//test for winner not to be equal to 'Kunle'.
+test("when andTheWinnerIs is called, 'And the winner of the 'gipo-ski award' is Kunle!' should NOT be displayed ", function () {
+  //arrange
+  const actual = andTheWinnerIs();
+  const expected = "And the winner of the 'gipo-ski award' is Kunle!";
+  //assert
+  expect(actual).not.toBe(expected);
+});
+
+//test for andTheWinnerIs function call.
+test("when andTheWinnerIs is called, 'And the winner of the 'gipo-ski award' is ABIgail!' should be displayed ", function () {
+  //arrange
+  const actual = andTheWinnerIs();
+  const expected = "And the winner of the 'gipo-ski award' is ABIgail!";
+  //assert
+  expect(actual).toBe(expected);
+});
