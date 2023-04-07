f4b893d3b563e2e500a8f20da46f62f700c3e9c5
gipo-ski <koladiji@gmail.com>
2023-04-07 20:14:44 +0100

    completed part1 of step3, removed solution code from kata of make-your-own-codewars-recap-tasks

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 👉 Write your kata here!
 "And the winner of the 'gipo-ski award' is..."
 
-you are the host of big-g show. its your job to announce the winner of the 'gipo-ski award'.
+you are the host of the big-g show. its your job to announce the winner of the 'gipo-ski award'.
 there are 8 possible winners: Michael, Abigail, Sofia, Sebastian, Noah, Evangeline, David or Kunle.
 only one person could be named winner.
 
@@ -16,25 +16,7 @@ the first 3 letters of winner's name should be in capital letters.
 //👉 Write the function your CodeWarriors will start with below here:
 export function andTheWinnerIs() {
   //write your code here.
-  //store nominees in an array
-  const nominees = [
-    "Michael",
-    "Abigail",
-    "Sofia",
-    "Sebastian",
-    "Noah",
-    "Evangeline",
-    "David",
-    "Kunle",
-  ];
-  //sort the array
-  const nomineesSorted = nominees.sort();
-  //slice() winner name after 3rd character of name
-  const restOfWinnerName = nomineesSorted[0].slice(3);
-  //slice() 1st 3 characters of winner name, capitalise it and concatinate restOfWinnerName to it
-  const winner = nomineesSorted[0].slice(0, 3).toUpperCase() + restOfWinnerName;
-  //get result
-  return `And the winner of the 'gipo-ski award' is ${winner}!`;
+  
+  //result should read `And the winner of the 'gipo-ski award' is ${winner}!`;
 }
-// winner = andTheWinnerIs();
-console.log(andTheWinnerIs());
+
